0a0b966f6539ed706d02b687eb27e861d0b95489
busraayar <busrayar127@gmail.com>
2023-08-19 17:28:23 +0300

    committed 2

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const ejs = require('ejs');
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//ROUTERS
 app.get("/", (req, res) => {
-  res.send("index sayfası").status(200);
+  res.status(200).render('index', {
+    page_name: "index"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render('about', {
+    page_name: "about"
+  });
 });
 
 const port = 3000;
